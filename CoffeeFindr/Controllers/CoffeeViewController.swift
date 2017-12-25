3322786a800071a3a58c1bd6f513bb600d7edb81
Jeremy <jbader@conncoll.edu>
2017-12-25 16:52:52 -0600

    Now just getting nearby coffee shops

--- CoffeeFindr/Controllers/CoffeeViewController.swift
+++ CoffeeFindr/Controllers/CoffeeViewController.swift
@@ -32,22 +32,18 @@ class CoffeeViewController: UIViewController, CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status == .authorizedWhenInUse {
-            print("User allowed us to access location")
-            //do whatever init activities here.
+            print("User allowed us to access their location when app is in use.")
         }
         else if status == .authorizedAlways {
-            print("User allowed us to always access location")
-            //do whatever init activities here.
+            print("User allowed us to always access their location.")
         }
     }
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
-        print("locations = \(locValue.latitude) \(locValue.longitude)")
-        
+    func getNearbyCoffeeShops(locValue:CLLocationCoordinate2D){
         let parameter: [String: String] = [
             "ll": "\(locValue.latitude), \(locValue.longitude)",
             "limit": "10",
+            "categoryId": "4bf58dd8d48988d1e0931735"
             ];
         
         let client = FoursquareAPIClient(clientId: "GRIFNKPCDBBZDZGHGCUQV4QYPVVVWBVTOSWIHDWRACW4AHD1", clientSecret: "4CVVZTW55PDL5IUYO1W35OZ5KZA4O2Q01LZPDMGSIFTQU25Y")
@@ -55,22 +51,27 @@ class CoffeeViewController: UIViewController, CLLocationManagerDelegate {
         client.request(path: "venues/search", parameter: parameter) { result in
             switch result {
             case let .success(data):
-                // parse the JSON data with NSJSONSerialization or Lib like SwiftyJson
-                // e.g. {"meta":{"code":200},"notifications":[{"...
                 let json = try! JSONSerialization.jsonObject(with: data, options: [])
                 print(json)
                 
             case let .failure(error):
-                // Error handling
                 switch error {
                 case let .connectionError(connectionError):
                     print(connectionError)
                 case let .apiError(apiError):
-                    print(apiError.errorType)   // e.g. endpoint_error
-                    print(apiError.errorDetail) // e.g. The requested path does not exist.
+                    print(apiError.errorType)
+                    print(apiError.errorDetail)
                 }
             }
         }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        
+        getNearbyCoffeeShops(locValue: locValue)
+        
         locationManager.stopUpdatingLocation()
     }
     
