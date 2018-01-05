191516e5582ece3a1a8f724200e895e53a982f50
Jeremy <jbader@conncoll.edu>
2018-01-05 16:27:29 -0600

    Some polish on both screens

--- CoffeeFindr/Controllers/CoffeeShopController.swift
+++ CoffeeFindr/Controllers/CoffeeShopController.swift
@@ -14,11 +14,14 @@ import FoursquareAPIClient
 
 class CoffeeShopController: UIViewController {
     @IBOutlet weak var coffeeShopImageView: UIImageView!
-    @IBOutlet weak var addressLabel: UILabel!
-    @IBOutlet weak var phoneNumberLabel: UILabel!
+    @IBOutlet weak var addressLabel: UITextView!
+    @IBOutlet weak var phoneNumberLabel: UITextView!
     @IBOutlet weak var daysLabel: UILabel!
     @IBOutlet weak var hoursLabel: UILabel!
     @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var addressView: UIView!
+    @IBOutlet weak var phoneNumberView: UIView!
+    @IBOutlet weak var hoursView: UIView!
     
     var coffeeShop: CoffeeShop?
     var locValue: CLLocationCoordinate2D?
@@ -28,6 +31,12 @@ class CoffeeShopController: UIViewController {
         let backItem = UIBarButtonItem()
         backItem.title = ""
         self.navigationController!.navigationBar.topItem!.backBarButtonItem = backItem
+        
+        addressView.layer.addBorder(edge: UIRectEdge.top, color: UIColor.lightGray, thickness: 1.0)
+        phoneNumberView.layer.addBorder(edge: UIRectEdge.top, color: UIColor.lightGray, thickness: 1.0)
+        hoursView.layer.addBorder(edge: UIRectEdge.top, color: UIColor.lightGray, thickness: 1.0)
+        hoursView.layer.addBorder(edge: UIRectEdge.bottom, color: UIColor.lightGray, thickness: 1.0)
+        
         getCoffeeShopDetails()
     }
     
@@ -50,7 +59,12 @@ class CoffeeShopController: UIViewController {
                 
                 print(json)
                 
-                self.descriptionTextView.text = json["response"]["venue"]["description"].string!
+                if let description = json["response"]["venue"]["description"].string
+                {
+                    self.descriptionTextView.text = description
+                } else {
+                    self.descriptionTextView.text = "No Description Available"
+                }
                 
                 let addressLabel1 = json["response"]["venue"]["location"]["formattedAddress"][0].string!
                 let addressLabel2 = json["response"]["venue"]["location"]["formattedAddress"][1].string!
@@ -94,4 +108,34 @@ class CoffeeShopController: UIViewController {
             coffeeShopImageView.sd_setImage(with: URL(string: string.replacingOccurrences(of: "\"", with: "", options: NSString.CompareOptions.literal, range:nil)), placeholderImage: UIImage(named: ""))
         }
     }
+}
+
+extension CALayer {
+    
+    func addBorder(edge: UIRectEdge, color: UIColor, thickness: CGFloat) {
+        
+        let border = CALayer();
+        
+        switch edge {
+        case UIRectEdge.top:
+            border.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: thickness)
+            break
+        case UIRectEdge.bottom:
+            border.frame = CGRect(x:0, y:self.frame.height - thickness, width:self.frame.width, height:thickness)
+            break
+        case UIRectEdge.left:
+            border.frame = CGRect(x:0, y:0, width: thickness, height: self.frame.height)
+            break
+        case UIRectEdge.right:
+            border.frame = CGRect(x:self.frame.width - thickness, y: 0, width: thickness, height:self.frame.height)
+            break
+        default:
+            break
+        }
+        
+        border.backgroundColor = color.cgColor;
+        
+        self.addSublayer(border)
+    }
+    
 }
